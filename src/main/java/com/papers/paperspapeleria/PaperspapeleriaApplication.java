523eada8316bd6c37ebc58d95a9684083980f6a5
Valeriatm06 <valeria.tocarruncho@uptc.ecu.co>
2025-11-05 21:53:08 -0500

    initial commit for the proyect

---
+++ src/main/java/com/papers/paperspapeleria/PaperspapeleriaApplication.java
@@ -0,0 +1,13 @@
+package com.papers.paperspapeleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperspapeleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperspapeleriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papers/paperspapeleria/PaperspapeleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papers.paperspapeleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperspapeleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
